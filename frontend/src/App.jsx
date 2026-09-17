d8bcde107a4edc5650c6b6717f6ea9e798df7bee
674259018-wq <674259018@webmail.npru.ac.th>
2026-09-17 16:32:01 +0700

    first

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import heroImg from "./assets/hero.png";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <div className="aura">
+        <div className="card bg-base-100">
+          <div className="card-body">
+            <p>This card has aura</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
